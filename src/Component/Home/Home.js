d1bda5ea9209e9c19b4ec3cc45d287c244ffff18
sadik126 <sadikhimel04@gmail.com>
2022-06-09 19:54:09 +0600

    m

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,6 +4,7 @@ import Banner from '../Banner/Banner';
 import Header from '../Header/Header';
 import Portfolio from '../Portfolio/Portfolio';
 import Projectsnumber from '../Projectsnumber/Projectsnumber';
+import Reviews from '../Reviews/Reviews';
 import Works from '../Works/Works';
 
 
@@ -16,6 +17,7 @@ const Home = () => {
             <Projectsnumber></Projectsnumber>
             <Works></Works>
             <Portfolio></Portfolio>
+            <Reviews></Reviews>
 
         </div>
     );
