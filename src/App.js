1e4705b2b1cef1946d7f7ce28c25ed46ea23cc23
sadik126 <sadikhimel04@gmail.com>
2022-06-09 06:45:51 +0600

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+import Home from './Component/Home/Home';
+import Service from './Component/Service/Service';
+import Experience from './Component/Experience/Experience';
+import Portfolio from './Component/Portfolio/Portfolio';
+
 function App() {
   return (
-    <div >
-      i am here
-    </div>
+    <Routes>
+      <Route path='/' element={<Home></Home>}>
+
+
+
+      </Route>
+      <Route path='/service' element={<Service></Service>}></Route>
+      <Route path='/experience' element={<Experience></Experience>}></Route>
+      <Route path='/portfolio' element={<Portfolio></Portfolio>}></Route>
+    </Routes>
   );
 }
 
