3813d1651b2c3fba02e02fe18467a9457c2f51ba
sadik126 <sadikhimel04@gmail.com>
2022-06-12 07:08:34 +0600

    m

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -4,6 +4,7 @@ import { themeContext } from '../../Context';
 import Toggle from '../Toggle/Toggle';
 import './Header.css';
 import { Link } from "react-scroll";
+import s from '../../img/s..png'
 
 const Header = () => {
     const theme = useContext(themeContext);
@@ -12,17 +13,17 @@ const Header = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light "  >
             <div class="container-fluid">
-                <Link class="navbar-brand fs-2 fw-bolder" to="/" style={{ color: darkMode ? "white" : "" }}>Sadik</Link>
+                <Link class="navbar-brand fs-2 fw-bolder" to="/" style={{ color: darkMode ? "white" : "" }}> <img src={s} alt="" /><span style={{ fontWeight: "bold", fontSize: '32px' }}>ADIK</span> </Link>
                 <Toggle></Toggle>
                 <button class="navbar-toggler bg-light" style={{ color: darkMode ? "white" : "" }} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon" style={{ color: darkMode ? "white" : "" }}></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav align-items-center ms-auto mb-2 mb-lg-0" >
-                        <li class="nav-item me-3 py-2">
+                        <li class="nav-item me-3">
                             <a class="nav-link" style={{ color: darkMode ? "white" : "" }} aria-current="page" href="/">Home</a>
                         </li>
-                        <li class="nav-item me-3 py-2">
+                        <li class="nav-item me-3 ">
                             <a class="nav-link" style={{ color: darkMode ? "white" : "" }} href="/blog" >Blogs</a>
                         </li>
                         <li class="nav-item me-3">
